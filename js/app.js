5ec8a8e38bcece1321723df69a5cad92f079e59a
zn3sky <zn3sky@gmail.com>
2017-03-09 19:43:44 +0900

    test ok

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ $(function() {
 	noteView.render().$el.appendTo($(document.body));
 });
 */
-/*親view動作確認コード*/;
+/*親view動作確認コード
 $(function() {
 	var noteCollection = new App.NoteCollection([{
 		title: "てすと１",
@@ -33,3 +33,54 @@ $(function() {
 
 	mainContainer.show(noteListView);
 })
+*/;
+
+// test用
+var initializeNotes = function() {
+	var noteCollection = new App.NoteCollection([{
+		title: "てすと１",
+		body: "てすと１です"
+	}, {title: "てすと２",
+		body: "てすと２です"
+	}, {title: "てすと３",
+		body: "てすと３です"
+	}]);
+
+	noteCollection.each(function(note) {
+		note.save();
+	});
+
+	return noteCollection.models;
+};
+
+$(function() {
+	App.noteCollection = new App.NoteCollection();
+	
+	App.mainContainer = new App.Container({
+		el: "#main-container"
+	});
+	
+	App.headerContainer = new App.Container({
+		el: "#header-container"
+	});
+	
+	App.noteCollection.fetch().then(function(notes) {
+		if (notes.length === 0) {
+			var models = initializeNotes();
+			App.noteCollection.reset(models);
+		}
+		var mainContainer = new App.Container({
+			el: "#main-container"
+		});
+
+		var noteListView = new App.NoteListView({
+			collection: App.noteCollection
+		});
+
+		App.mainContainer.show(noteListView);
+		
+		App.router = new App.Router();
+		Backbone.history.start();
+	});
+
+});
